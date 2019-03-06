7438b60e3563145280e366489329e173d45cc8c9
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-03-06 15:50:32 +0100

    Inizio attività

---
+++ Proximity-and-DHT11/Proximity-and-DHT11.ino
@@ -0,0 +1,41 @@
+#include "DHT.h"
+#include <HCSR04.h>
+
+int TrigPin = 5;
+int EchoPin = 6;
+UltraSonicDistanceSensor distanceSensor(TrigPin, EchoPin);
+
+#define DHTPIN 10
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long old_millis = 0;
+int delta = 1000;
+int ledG = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledG, OUTPUT);
+
+  dht.begin();
+  Serial.println("Time, Humidity, Temperature, Distance");
+
+}
+
+void loop() {
+  unsigned long now = millis();
+
+  if(now >= old_millis + delta){
+    digitalWrite(ledG, HIGH);
+    Serial.print(now/1000);
+    Serial.print(", ");
+    Serial.print(dht.readHumidity());
+    Serial.print("%, ");
+    Serial.print(dht.readTemperature());
+    Serial.print(", ");
+    Serial.println(distanceSensor.measureDistanceCm());
+    digitalWrite(ledG, LOW);
+    old_millis = now;
+  }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Proximity and DHT11
+
+
+Esercizio per programmare Arduino UNO per misurare una distanza di prossimità, temperatura e umidità.
+
+Componenti:
+_Arduino UNO
+_DHT11
+_HC SR04
+_2 LED red
+_1 LED green
+_Button
