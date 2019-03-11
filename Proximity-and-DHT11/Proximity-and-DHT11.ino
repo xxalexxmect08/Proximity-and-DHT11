f4349b926577c5a55d68ce4d09beb6eb3b87d5f3
xxalexxmect08 <xxalexxmect08@users.noreply.github.com>
2019-03-11 14:36:34 +0100

    Modificato il programma per utilizzo con soglia su distanza da potenziometro

--- Proximity-and-DHT11/Proximity-and-DHT11.ino
+++ Proximity-and-DHT11/Proximity-and-DHT11.ino
@@ -15,6 +15,7 @@ int ledG = 13;            //definire pin led Green
 int ledR1 = 11;           //definire pins leds Red
 int ledR2 = 12;
 int button = 2;           //definire pin pulsante
+int pot = A5;              //definire pin potenziometro
 
 void setup() {
   Serial.begin(9600);        //avvio seriale
@@ -23,29 +24,35 @@ void setup() {
   pinMode(ledR2, OUTPUT);
   pinMode(button, INPUT);
   dht.begin();         //avvio DHT
-  Serial.println("Time, % Humidity, Temperature, Distance");     //scrittura indice per dati
+  Serial.println("Time, % Humidity, Temperature, Distance,Soglia");     //scrittura indice per dati
 }
 void loop() {
   unsigned long now = millis();
+      int avg = 0;                                  //media distanza su 10 valori a 5 ms
+    int number = 25;
+    for (int i=0;i<number;i++) {
+    if(distanceSensor.measureDistanceCm()!= -1){
+      avg += distanceSensor.measureDistanceCm();
+    }
+    delay(10);
+   }
+   int m = avg / 25;
+   int X = map(analogRead(pot),0,1023,4,400);          //mappatura del potenziometro
+  if(m > X){                             //confronto tra distanza e valore mappato
   if(now >= old_millis + delta){          //se è passato il tempo definito da delta
     old_millis = now;
     digitalWrite(ledG, HIGH);             //accendi led green
     float h = dht.readHumidity();         //leggi umidità
     float t = dht.readTemperature();      //leggi temperatura
-    int avg = 0;                                  //media distanza su 10 valori a 5 ms
-    int number = 10;
-    for (int i=0;i<number;i++) {
-    avg += distanceSensor.measureDistanceCm();
-    delay(5);
-   }
-   int m = avg / 10;
     Serial.print(now/1000);                 //scrivi info in seriale
     Serial.print(", ");
     Serial.print(h);
     Serial.print(", ");
     Serial.print(t);
     Serial.print(", ");
-    Serial.println(m);
+    Serial.print(m);
+    Serial.print(", ");
+    Serial.println(X);
     digitalWrite(ledG, LOW);     //spegni led green
     if(h<50){             //se umidità minore 50%
       digitalWrite(ledR1, HIGH);       //accendi led red1
@@ -53,6 +60,7 @@ void loop() {
     if(t>30){                  //se temperatura superiore 30°C
       digitalWrite(ledR2, HIGH);      //accendi led red2
     }
+  }
   }
    if(digitalRead(button)){     //se button premuto
     digitalWrite(ledR1, LOW);       //spegni leds red
